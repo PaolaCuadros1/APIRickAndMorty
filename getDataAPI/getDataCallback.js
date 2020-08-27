d0eab1244adc45a2d5e44966c9c5b39b4c1d29af
PaolaCuadros1 <paola.cuadroso@gmail.com>
2020-08-27 16:54:42 -0500

    Consumo de API, html y estilos.

---
+++ getDataAPI/getDataCallback.js
@@ -0,0 +1,8 @@
+console.log("Esto se maneja con callbacks")
+const API = 'https://rickandmortyapi.com/api/character/';
+
+function prepareData(apiURL, callback) {
+    callback(apiURL)
+}
+
+prepareData(API, getData);
---
+++ getDataAPI/getDataPromise.js
@@ -0,0 +1,4 @@
+console.log("Esto se maneja con promesas.")
+const API = 'https://rickandmortyapi.com/api/character/';
+
+getData(API);
